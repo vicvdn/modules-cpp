4d7dabb76aeab11696f3b5ecb50bde57eac4f9fe
vvaudain <vvaudain@student.42.fr>
2024-06-07 18:43:49 +0200

    ex01 done

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/07 14:55:00 by vvaudain          #+#    #+#             */
-/*   Updated: 2024/06/07 17:26:28 by vvaudain         ###   ########.fr       */
+/*   Created: 2024/06/07 16:21:12 by vvaudain          #+#    #+#             */
+/*   Updated: 2024/06/07 18:42:10 by vvaudain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 Fixed::Fixed( void ) : _number(0) {
 	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const int number ) {
+	std::cout << "Int constructor called" << std::endl;
+	this->_number = number << this->_frac_bits;
+}
+
+Fixed::Fixed (const float number) {
+	std::cout << "Float constructor called" << std::endl;
+	this->_number = roundf(number * (1 << this->_frac_bits));
 }
 
 Fixed::~Fixed() {
@@ -35,10 +45,22 @@ Fixed&	Fixed::operator=( Fixed const &rhs) {
 }
 
 int Fixed::getRawBits( void ) const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_number;
 }
 
 void Fixed::setRawBits( int const raw ) {
 	this->_number = raw;
+}
+
+float 	Fixed::toFloat( void ) const {
+	return ((float)(this->_number) / (float)(1 << this->_frac_bits));
+}
+
+int 	Fixed::toInt( void ) const {
+	return (this->_number >> this->_frac_bits);
+}
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs) {
+	o << rhs.toFloat();
+	return (o);
 }
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 16:16:56 by vvaudain          #+#    #+#             */
+/*   Updated: 2024/06/07 18:30:37 by vvaudain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int	_number;
+	static const int _frac_bits = 8;
+public:
+	Fixed( void );
+	Fixed (const int number);
+	Fixed (const float number);
+	Fixed(const	Fixed&	copy);
+	~Fixed();
+	Fixed&	operator=(Fixed const & rhs);
+	
+	int 	getRawBits( void ) const;
+	void 	setRawBits( int const raw );
+	float 	toFloat( void ) const;
+	int 	toInt( void ) const;
+};
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs);
+
+#endif
