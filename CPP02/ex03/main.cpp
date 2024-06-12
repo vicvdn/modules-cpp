839c0013d08ca092ae0dee1a1f0898aa93dccea4
vvaudain <vvaudain@student.42.fr>
2024-06-12 13:49:26 +0200

    ex03 done

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:06:20 by vvaudain          #+#    #+#             */
-/*   Updated: 2024/06/12 13:31:55 by vvaudain         ###   ########.fr       */
+/*   Updated: 2024/06/12 13:48:30 by vvaudain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 #include "Fixed.hpp"
 
 int	main(void) {
-	Point a(0, 0);
+	Point a;
 	Point b(2, 3);
 	Point c(3, 0);
 	Point point(1.5864f, 1);
+	Point point2(2.0f, 3.00f);
+	Point point3(3, 0);
 	Fixed test(3);
 	
 	std::cout << "a(x)" << a.get_x() << std::endl;
@@ -32,6 +34,20 @@ int	main(void) {
 	std::cout << "-------------------" << std::endl;
 	
 	if (bsp(a, b, c, point) == true)
+		std::cout << "TRUE" << std::endl;
+	else
+		std::cout << "FALSE" << std::endl;
+
+	std::cout << "-------------------" << std::endl;
+
+	if (bsp(a, b, c, point2) == true)
+		std::cout << "TRUE" << std::endl;
+	else
+		std::cout << "FALSE" << std::endl;
+
+	std::cout << "-------------------" << std::endl;
+
+	if (bsp(a, b, c, point3) == true)
 		std::cout << "TRUE" << std::endl;
 	else
 		std::cout << "FALSE" << std::endl;
