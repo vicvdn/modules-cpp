f9bf2931825703ab84fe60895ebcaf9b43a1d7b0
Victoire <victoirevdn@gmail.com>
2024-10-17 14:26:35 +0200

    CPP07 ex00

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victoirevaudaine <victoirevaudaine@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:02:51 by victoirevau       #+#    #+#             */
+/*   Updated: 2024/10/17 14:25:54 by victoirevau      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+void printSeparator()
+{
+	std::cout << "---------------------------------" << std::endl;
+}
+
+template <typename T> void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T> T min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T> T max(T &a, T &b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+
+	std::cout << BOLDGREEN << "a = " << a << "," << BOLDYELLOW << " b = " << b << RESET << std::endl;
+	printSeparator();
+	std::cout << BOLDBLUE << "Let's swap them!" << RESET << std::endl;
+	swap(a, b);
+	printSeparator();
+	std::cout << BOLDGREEN << "a = " << a << "," << BOLDYELLOW <<  "b = " << b << RESET << std::endl;
+	std::cout << "min(a, b) = " << min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << max(a, b) << std::endl;
+	
+	std::cout << std::endl;
+	std::cout << std::endl;
+	
+	std::string c = "blabla1";
+	std::string d = "blabla2";
+
+	std::cout << BOLDGREEN << "c = " << c << "," << BOLDYELLOW << "d = " << d << RESET << std::endl;
+	printSeparator();
+	std::cout << BOLDBLUE << "Let's swap them!" << RESET << std::endl;
+	swap(c, d);
+	printSeparator();
+	std::cout << BOLDGREEN << "c = " << c << "," << BOLDYELLOW << "d = " << d << RESET << std::endl;
+	std::cout << "min(c, d) = " << min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << max(c, d) << std::endl;
+
+	return 0;
+}
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victoirevaudaine <victoirevaudaine@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:03:37 by victoirevau       #+#    #+#             */
+/*   Updated: 2024/10/17 14:13:23 by victoirevau      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+#define BOLDBLUE	"\033[1m\033[34m"
+#define BOLDGREEN	"\033[1m\033[32m"
+#define BOLDRED		"\033[1m\033[31m"
+#define BOLDYELLOW	"\033[1m\033[33m"
+#define RESET		"\033[0m"
+
+template <typename T> void swap(T &a, T &b);
+template <typename T> T min(T &a, T &b);
+template <typename T> T max(T &a, T &b);
+
+#endif
